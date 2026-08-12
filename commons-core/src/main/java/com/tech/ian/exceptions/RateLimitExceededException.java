29346d59d47333e95afc48f5a81a7397f0c4c7e7
IanLemee <ianlemee@hotmail>
2026-08-12 19:17:06 -0300

    feat: addition of module commons-core for Exceptions

--- commons-core/src/main/java/com/tech/ian/exceptions/RateLimitExceededException.java
+++ commons-core/src/main/java/com/tech/ian/exceptions/RateLimitExceededException.java
@@ -1,4 +1,4 @@
-package com.tech.ian.tokenbucket.service.ex;
+package com.tech.ian.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
