ed3be12fee2e4c7a9a129680cf007a52e6007d59
IanLemee <ianlemee@hotmail>
2026-08-19 07:57:15 -0300

    fix: fixing some race conditions and thread safity issues on algorithms

--- token-bucket/src/main/java/com/tech/ian/tokenbucket/service/TokenBucketService.java
+++ token-bucket/src/main/java/com/tech/ian/tokenbucket/service/TokenBucketService.java
@@ -11,36 +11,29 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class TokenBucketService {
 
-    private final Map<String, List<Integer>> map = new ConcurrentHashMap<>();
+    private final Map<String, AtomicInteger> map = new ConcurrentHashMap<>();
     private static final Logger LOGGER = LoggerFactory.getLogger(TokenBucketService.class);
-
+    private static final int MAX_TOKENS = 5;
     public void processRequest(String name) {
-        if (map.containsKey(name)) {
-            List<Integer> integers = map.get(name);
-            if (integers.isEmpty()) {
-                LOGGER.warn("bucket for user: {} is empty, throwing an exception", name);
-                throw new RateLimitExceededException();
-            }
-            integers.removeLast();
-            LOGGER.info("removing token from bucket for user: {}, tokens remaining {}", name, integers.size());
-            return;
+        AtomicInteger tokens = map.computeIfAbsent(name, k -> new AtomicInteger(MAX_TOKENS));
+
+        if (tokens.get() <= 0) {
+            throw new RateLimitExceededException();
         }
-
-        map.put(name, new ArrayList<>(List.of(1,1,1,1,1)));
-        LOGGER.info("creating token bucket for user: {}", name);
-
-        List<Integer> integers = map.get(name);
-        integers.removeLast();
-        LOGGER.info("removing token from bucket for user: {}, tokens remaining {}", name, integers.size());
+        tokens.decrementAndGet();
     }
 
     @Scheduled(cron = "*/10 * * * * *")
     public void refillBucket() {
-        Collection<List<Integer>> values = map.values();
-        values.stream().filter(integers -> integers.size() < 5).forEach(integers -> integers.add(1));
+        map.values().forEach(tokens -> {
+            if (tokens.get() < MAX_TOKENS) {
+                tokens.incrementAndGet();
+            }
+        });
     }
 }
